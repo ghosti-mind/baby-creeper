f499c2d0ee64c398e4494f1dc6c310cfb7adfc9a
ghosti <ghosti.thoughts@gmail.com>
2026-09-19 14:36:21 -0500

    Fix baby creeper texture

--- src/client/java/ghosti/babycreeper/client/BabyCreeperRenderer.java
+++ src/client/java/ghosti/babycreeper/client/BabyCreeperRenderer.java
@@ -2,15 +2,12 @@ package ghosti.babycreeper.client;
 
 import ghosti.babycreeper.BabyCreeper;
 import ghosti.babycreeper.BabyCreeperEntity;
-import ghosti.babycreeper.client.BabyCreeperModel;
-import net.minecraft.client.model.geom.ModelLayers;
-import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.EntityRendererProvider.Context;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.resources.Identifier;
 
 public class BabyCreeperRenderer extends MobRenderer<BabyCreeperEntity, BabyCreeperRenderState, BabyCreeperModel> {
-	private static final Identifier CREEPER_LOCATION = Identifier.fromNamespaceAndPath(BabyCreeper.MOD_ID, "resources/baby-creeper/baby_creeper.png");
+	private static final Identifier CREEPER_LOCATION = Identifier.fromNamespaceAndPath(BabyCreeper.MOD_ID, "textures/entity/baby_creeper.png");
 
     public BabyCreeperRenderer(Context context) {
         // model
